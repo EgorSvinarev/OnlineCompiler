fa65bee12e82ce3457a8f582787a01afdee0efcf
Egor Svinarev <egor-rdd@mail.ru>
2021-10-14 15:33:56 +0400

    Limits import was removed from a code

---
+++ src/test/java/com/svinarev/compiler/converters/RawCodeConverterTest.java
@@ -0,0 +1,31 @@
+package com.svinarev.compiler.converters;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.svinarev.compiler.converters.RawCodeConverter;
+import com.svinarev.compiler.models.RawCode;
+import com.svinarev.compiler.dto.RawCodeDTO;
+
+
+public class RawCodeConverterTest extends BaseConverterTest {
+
+	@Test
+	public void fromDTO() {
+		RawCodeDTO rawCodeDto = getRawCodeDto();
+		RawCode rawCode = RawCodeConverter.fromDTO(rawCodeDto);
+		
+		assertEquals("import limits\n" + rawCodeDto.getCode(), rawCode.getCode());
+	}
+	
+	@Test
+	public void toDTO() {
+		RawCode rawCode = getRawCode();
+		RawCodeDTO rawCodeDto = RawCodeConverter.toDTO(rawCode);
+		
+		assertEquals("import limits\n" + rawCodeDto.getCode(), rawCode.getCode());
+	}
+	
+}
