5c1bd3d97aa536ea333aef4564a950325bc182ab
Egor Svinarev <egor-rdd@mail.ru>
2021-10-15 12:28:42 +0400

    The Whitelisting ip for connections was implemented.

---
+++ src/main/java/com/svinarev/compiler/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.svinarev.compiler.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import com.svinarev.compiler.utils.FileHandler;
+import com.svinarev.compiler.filters.WhitelistProcessingFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Value("${whitelist.file}")
+	private String whitelistFile;
+	
+	@Autowired
+	private FileHandler fileHandler;
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		WhitelistProcessingFilter whitelistFilter = new WhitelistProcessingFilter(whitelistFile, fileHandler);
+		whitelistFilter.setAuthenticationManager(authenticationManagerBean());
+		
+		PreAuthenticatedAuthenticationProvider preAuthProvider = new PreAuthenticatedAuthenticationProvider();
+        preAuthProvider.setPreAuthenticatedUserDetailsService(new UserDetailsByNameServiceWrapper<>(whitelistFilter));
+        http.authenticationProvider(preAuthProvider);
+	
+        http
+        	.antMatcher("/compile")
+    		.addFilterAt(whitelistFilter, AbstractPreAuthenticatedProcessingFilter.class);
+        
+		http.csrf().disable()
+	    	.sessionManagement().sessionCreationPolicy(STATELESS)
+	    .and()
+	    	.authorizeRequests()
+	    		.antMatchers("/swagger-ui/**").permitAll()
+	    		.antMatchers("/compile").authenticated();
+	    
+	}
+	
+}
---
+++ src/main/java/com/svinarev/compiler/filters/WhitelistProcessingFilter.java
@@ -0,0 +1,67 @@
+package com.svinarev.compiler.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.Collections;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.svinarev.compiler.utils.FileHandler;
+
+public class WhitelistProcessingFilter extends AbstractPreAuthenticatedProcessingFilter implements UserDetailsService {
+	
+	private Logger logger = LoggerFactory.getLogger(WhitelistProcessingFilter.class);
+	private String[] allowedIP;
+	private String whitelistFile;
+	private FileHandler fileHandler;
+	
+	public WhitelistProcessingFilter(String whitelistFilePath, FileHandler fileHandler) {
+		this.whitelistFile = whitelistFilePath;
+		this.fileHandler = fileHandler;
+		
+		this.allowedIP = getWhitelistAddresses();
+	}
+	
+	@Override
+	protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+		for (String ip: allowedIP) {
+			if (new IpAddressMatcher(ip).matches(request)) {
+				logger.debug("Remote IP is in the allowed list: {}", request.getRemoteAddr(), allowedIP);
+				return request.getRemoteAddr();
+			}
+		}
+		
+		logger.debug("Remote IP {} isn't in the allowed list: {}", request.getRemoteAddr(), allowedIP);
+		
+		return null;
+	}
+	
+	@Override
+    protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
+        return "N/A";
+    }
+	
+	@Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User(username, "N/A", Collections.emptyList());
+    }
+
+	
+	private String[] getWhitelistAddresses() {
+		String list = fileHandler.read(whitelistFile);
+		
+		String[] addresses = list.split("\n");
+		return addresses;
+	}
+	
+}
