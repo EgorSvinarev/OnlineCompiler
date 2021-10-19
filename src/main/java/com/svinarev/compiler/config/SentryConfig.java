7e4aa06a83dcb31cddce7d3a009a0fc898cf9e14
Egor Svinarev <egor-rdd@mail.ru>
2021-10-20 00:04:20 +0400

    sentry configuration

---
+++ src/main/java/com/svinarev/compiler/config/SentryConfig.java
@@ -0,0 +1,10 @@
+package com.svinarev.compiler.config;
+
+import io.sentry.spring.EnableSentry;
+import org.springframework.context.annotation.Configuration;
+
+@EnableSentry(dsn = "https://dcc2cc37d6ac45da92c70371a80cbcb3@o624741.ingest.sentry.io/6020006")
+@Configuration
+public class SentryConfig {
+
+}
