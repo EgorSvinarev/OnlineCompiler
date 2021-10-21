5a4af3d39ef16ab03a40e0386050e446ec8ffaaa
Egor Svinarev <egor-rdd@mail.ru>
2021-10-21 10:57:46 +0400

    changed property's name

--- src/main/java/com/svinarev/compiler/config/SentryConfig.java
+++ src/main/java/com/svinarev/compiler/config/SentryConfig.java
@@ -3,7 +3,7 @@ package com.svinarev.compiler.config;
 import io.sentry.spring.EnableSentry;
 import org.springframework.context.annotation.Configuration;
 
-@EnableSentry(dsn = "${sentry.sdn}")
+@EnableSentry(dsn = "${sentry.dsn}")
 @Configuration
 public class SentryConfig {
 
