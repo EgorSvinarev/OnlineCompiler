78ab9afaa56fe15214a8d8690acd981a62bdfa61
Egor Svinarev <egor-rdd@mail.ru>
2021-10-23 03:08:59 +0400

    The method that adds the sct code

--- src/main/java/com/svinarev/compiler/utils/CodeFormatter.java
+++ src/main/java/com/svinarev/compiler/utils/CodeFormatter.java
@@ -3,6 +3,7 @@ package com.svinarev.compiler.utils;
 import org.springframework.stereotype.Component;
 
 import com.svinarev.compiler.models.RawCode;
+import com.svinarev.compiler.entities.Exercise;
 
 @Component
 public class CodeFormatter {
@@ -15,6 +16,20 @@ public class CodeFormatter {
 			   .build();
 	}
 	
-	
+	public RawCode prepareSCT(RawCode code, Exercise exercise) {
+		String sct = String.format("%s\n", exercise.getPreExerciseCode())
+					+ "from pythonwhat.test_exercise import prep_context\n"
+					+ "_, ctxt = prep_context()\n"
+					+ "globals().update(ctxt)\n"
+					+ "\n"
+					+ "from pythonwhat.test_exercise import setup_state\n"
+					+ String.format("setup_state(stu_code = \"\"\"%s\"\"\", sol_code = \"\"\"%s\"\"\")\n", code.getCode(), exercise.getSolution())
+					+ String.format("%s", exercise.getSct());
+		
+		return RawCode.builder()
+					.code(sct)
+			   .build();
+		
+	}
 	
 }
