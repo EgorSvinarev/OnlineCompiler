18d0c62b0162b0e33826c188d69dad79d436cbbb
Egor Svinarev <egor-rdd@mail.ru>
2021-11-01 17:53:55 +0400

    User entity

---
+++ src/main/java/com/svinarev/compiler/entities/User.java
@@ -0,0 +1,23 @@
+package com.svinarev.compiler.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class User {
+
+	@Id
+	private Long id;
+	
+}
