54dbea720afba4251ef92e0a4eb0cc4863aeed54
Egor Svinarev <egor-rdd@mail.ru>
2022-01-21 15:16:16 +0400

    Bug fix

--- src/main/java/com/svinarev/compiler/models/ExecutionResult.java
+++ src/main/java/com/svinarev/compiler/models/ExecutionResult.java
@@ -5,6 +5,9 @@ import lombok.NoArgsConstructor;
 import lombok.Data;
 import lombok.Builder;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -31,23 +34,46 @@ public class ExecutionResult {
 	}
 	
 	public static String parseShellFeedback(String feedback) {
-		/* Index of the feedback's begin */
-		int begin = feedback.indexOf("In [1]: ") + 8;
-		if (begin == 7) return feedback;
 		
+		String regex = "In\\s\\[\\d+\\]\\:";
+		Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+		
+		/* Сropping the header */
+		Matcher matcher = pattern.matcher(feedback);
+		
+		if (!matcher.find()) {
+			return feedback;
+		}
+		
+		int begin = matcher.end() + 1;
 		String tmp = feedback.substring(begin);
 		
-		/* Index of useless tail */
-		int end = tmp.lastIndexOf(" Do you really want to exit ([y]/n)? \nkeeping kernel alive") - 9;
-		/* If the feedback is empty then we return an empty string */
-		if (end == -1) end = 0;
-		/* The case when the useless tail wasn't found */
-		if (end == -9) return tmp;
+		/* cropping the tail */
+		regex = "In\\s\\[\\d+\\]\\:\\sDo\\syou really want to exit \\(\\[y\\]\\/n\\)\\? ";
+		pattern = Pattern.compile(regex, Pattern.MULTILINE);
+		matcher = pattern.matcher(tmp);
+		
+		if (!matcher.find()) {
+			return tmp;
+		}
+		
+		int end = matcher.start(0);
+		end = (end > 1) ? end - 2 : end - 1;
 		
 		tmp = tmp.substring(0, end);
-		if (tmp.length() > 0 && tmp.charAt(tmp.length() - 1) =='\n') {
-			return tmp.substring(0, tmp.length() - 1);
-		}
+		
+		/* cropping in/out supplements */
+		regex = "In\\s\\[\\d+\\]\\:";
+		pattern = Pattern.compile(regex, Pattern.MULTILINE);
+		matcher = pattern.matcher(tmp);
+		
+		if (matcher.find()) return tmp.substring(matcher.end() + 1);
+		
+		regex = "Out\\[\\d+\\]\\:\\s";
+		pattern = Pattern.compile(regex, Pattern.MULTILINE);
+		matcher = pattern.matcher(tmp);
+		
+		if (matcher.find()) return tmp.substring(matcher.end());
 		
 		return tmp;
 	}
